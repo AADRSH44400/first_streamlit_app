5cc2ab2f9f71fadf3a8c3ae870031dbc97a4bafb
AADRSH <118899222+AADRSH44400@users.noreply.github.com>
2022-11-29 11:38:27 +0530

    Update streamlit_app.py
    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('Hey, Adrash this side!')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
