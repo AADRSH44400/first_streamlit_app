5648920829ebc4a2f5329c1804a256299176de5d
AADRSH <118899222+AADRSH44400@users.noreply.github.com>
2022-12-01 16:39:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ streamlit.dataframe(my_fruit_list)
 
 
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','strawberries'])
